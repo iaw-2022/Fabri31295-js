8c1a3f9c8b7205a70a88f0871fac3b743d794ea4
Fabri31295 <fabricioalmaraz123@gmail.com>
2022-09-29 20:52:53 -0300

    Firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { CartProvider } from './context/cartContext'
 import NotificationProvider from './notifications/NotificationProvider'
 import { lazy, Suspense } from 'react';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import './App.css'
 
 const NavBar = lazy(() => import('./components/ui/navbar'))
@@ -11,6 +13,19 @@ const Store = lazy(() => import('./components/store/store'))
 const Cart = lazy(() => import('./components/cart/cart'))
 const Register = lazy(() => import('./components/register/register'))
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDBHZ9OZGKLHZkZDc0lLBam2OjhTv4-BiA",
+  authDomain: "webjs-f949b.firebaseapp.com",
+  projectId: "webjs-f949b",
+  storageBucket: "webjs-f949b.appspot.com",
+  messagingSenderId: "362341515411",
+  appId: "1:362341515411:web:1899ec6adb41277f7f8d66",
+  measurementId: "G-ZVNPCR36DY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,16 +3,27 @@ import About from "./about"
 import Contact from "./contact"
 import Curve from '../../resources/curva.png'
 import Links from "../ui/links"
+import { handleSubscription } from "../../subscription"
 import './style.css'
 
 const Home = () => {
-    return(
+
+    const componentDidMount = async() => {
+        try {
+           await handleSubscription(this.props.user)
+        } catch (error) {
+            console.log('Error in componentDidMount, error')
+        }
+    }
+
+    return (
         <div className="homeWrapper">
-            <Carousel/>
-            <About/>
-            <img id="curve" src={ Curve } alt="imagen decorativa"/>
-            <Contact/>
-            <Links/>
+            {componentDidMount}
+            <Carousel />
+            <About />
+            <img id="curve" src={Curve} alt="imagen decorativa" />
+            <Contact />
+            <Links />
         </div>
     )
 }
